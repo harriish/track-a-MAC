6abf84bd29c5801f25a843a2dc9ae7535bf3c8b7
ivaturi venkata harish kumar <43529736+harriish@users.noreply.github.com>
2019-10-10 23:30:52 +0200

    Create backend.py
    on the way......... create database first and import it............

---
+++ backend.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import easysnmp
+import time, datetime
+import threading
+import sqlite3
+import exceptions
+import sys
+
+
+
+class my_threads(threading.Thread) :
+    def __init__(self, ip, community, version, port):#apply OOPs concept , if fails start with function calls
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.community = community
+        self.version = version
+        self.port = port
+        
+    def run(self):
+        while True:#while-count to count number of failed attmepts
+            my_failed_attempts = 0
+            try:
+                session = Session(hostname=self.ip, community = self.community, version = self.version, timeout =5, retries=2)
+            except easysnmp.exceptions.EasySNMPTimeoutError:
+                my_failed_attempts += my_failed_attempts
+
+            
+            T = int(time.time())
+            probe = datetime.datetime.utcfromtimestamp(T)
+            next_probe = probe + datetime.timedelta(seconds=60)
+            #MACs VLAN oid?
