ff3672b1edb6a7dd6e884ea2fea15abf9ff7bc60
ivaturi venkata harish kumar <43529736+harriish@users.noreply.github.com>
2019-10-14 16:51:07 +0200

    performed snmpwalk

--- backend.py
+++ backend.py
@@ -28,4 +28,13 @@ class my_threads(threading.Thread) :
             T = int(time.time())
             probe = datetime.datetime.utcfromtimestamp(T)
             next_probe = probe + datetime.timedelta(seconds=60)
-            #MACs VLAN oid?
+            connector= sqlite3.Connection('mydatabase.db', timeout=7)
+            selector= connector.cursor()
+            selector.execute('''UPDATE status SET IP = ?,PORT=?,COMMUNITY=?,VERSION=?,FIRST_PROBE=?,LATEST_PROBE=?,Failed_attempts=? WHERE IP =?''',self.ip,self.port,self.community,self.version,probe,next_probe,failed_attempts,self.ip)
+            print('connected to device having ip address',self.ip,'at',probe)
+            connector.commit()
+
+            all = session.walk('1.3.6.1.2.1.17.4')#this command is used to perform an snmp walk to retrive address port an status of the networking device
+            ports = session.walk('IF-MIB::ifName')#where ifname is the oid value 1.3.6.1.2.1.31.1.1.1.1 which give us object type i.e. name of the interface to which the device is connected to
+            mac=[]
+            index_of_oid=[]
