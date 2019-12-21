4f2780f03a8b047b02be8d3ace865f05aa8e3a9b
ivaturi venkata harish kumar <43529736+harriish@users.noreply.github.com>
2019-12-21 19:27:47 +0100

    updating listDevs.php in a simple format

--- listDevs.php
+++ listDevs.php
@@ -8,5 +8,4 @@ while ($row = $statistics->fetchArray()){
 	echo $row['IP']. "|" .$row['PORT']. "|" .$row['COMMUNITY']. "|" .$row['VERSION']. "|" .$row['FIRST_PROBE']. "|" .$row['LATEST_PROBE']. "|" .$row['Failed_attempts']. "\n";
 }
 }
-
 ?>
