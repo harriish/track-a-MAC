914cfb0a98a28be3dd8041a1da98c341414c974f
ivaturi venkata harish kumar <43529736+harriish@users.noreply.github.com>
2019-12-21 19:27:12 +0100

    Update listDevs.php

--- listDevs.php
+++ listDevs.php
@@ -2,19 +2,11 @@
 
 include('config.php');
 
-$read = $db->query('SELECT count(*) FROM List');
-while($i = $read->fetchArray(SQLITE3_ASSOC)){
-
-	if ($i['count(*)']==0) {
-		echo "we didn't find any assosiated mac addresses ";
-	}
-
-else{
-$statistics = $db->query('SELECT * FROM List');
+$statistics = $db->query('SELECT * FROM info');
 
 while ($row = $statistics->fetchArray()){
 	echo $row['IP']. "|" .$row['PORT']. "|" .$row['COMMUNITY']. "|" .$row['VERSION']. "|" .$row['FIRST_PROBE']. "|" .$row['LATEST_PROBE']. "|" .$row['Failed_attempts']. "\n";
 }
 }
-}
+
 ?>
