1364ba669ed217475297b6b94a4eeefde405510b
ivaturi venkata harish kumar <43529736+harriish@users.noreply.github.com>
2019-12-21 17:28:06 +0100

    Update addDevice.php

--- addDevice.php
+++ addDevice.php
@@ -2,50 +2,33 @@
 
 include('config.php');
 
-if (empty($_GET)){
-    echo "none";
-    }
+$ip = $_GET['ip']
+$port = $_GET['port'];
+$community = $_GET['community'];
+$version = $_GET['version'];
+
+if(empty($ip) || empty($port) || empty($community) || empty($version)) {
+    echo "FALSE" ;   
+}
+
 else {
-    $ip = htmlspecialchars($_GET["ip"]);
-    $port = htmlspecialchars($_GET["port"]);
-    $community = htmlspecialchars($_GET["community"]);
-    $version = htmlspecialchars($_GET["version"]);
-      
-    $sqlite1 = <<<EOF
-        CREATE TABLE IF NOT EXISTS info (IP VARCHAR, PORT VARCHAR, COMMUNITY VARCHAR, VERSION VARCHAR);
-EOF;
-    $run1 = $db->exec($sqlite1);
-    if (!$run1) {
-        echo $db->lastErrorMsg();}#Returns text which describes failed SQLite request
-        
-        
-        $sqlite2 = <<<EOF
-        CREATE TABLE IF NOT EXISTS status (IP VARCHAR, PORT VARCHAR, COMMUNITY VARCHAR, VERSION VARCHAR, FIRST_PROBE VARCHAR, LATEST_PROBE VARCHAR, Failed_attempts INTEGER);
-EOF;
-    $run2 = $db->exec($sqlite2);
-    if (!$run2) {
-        echo $db->lastErrorMsg();}
+    $run1 = $db->query('SELECT * FROM info');
+    $count = 0;
+    foreach ($run1 as $run1) {
+        if($run1['ip']==$ip && $run1['port']==$port && $run1['community']==$community && $run1['version']==$version){
+            $count = $count+1;
 
-
-        $sql3 = <<<EOF
-        INSERT INTO info (IP,PORT,COMMUNITY,VERSION)
-        VALUES ('$ip','$port','$community','$version');
-EOF;
-      $run3 = $db->exec($sql3);
-      if (!$run3){
-        echo $db->LastErrorMsg();
-        } else {
-            echo "OK";
-            echo $ip,$port,$community,$version;
-              }
-    $sql4 = <<<EOF
-        INSERT INTO status (IP,PORT,COMMUNITY,VERSION)
-        VALUES ('$ip','$port','$community','$version');
-EOF;
-      $run4 = $db->exec($sql4);
-      if (!$run4){
-        echo $db->LastErrorMsg();
         }
-    $db->close();
     }
-    ?>
+
+    if($count==0){
+        $db->exec("INSERT INTO info (ip,port,community,version) VALUES ('$ip','$port','$community','$version')");
+        echo "OK";
+    }
+    else {
+        echo "FALSE";
+    }
+}
+$db->close();
+
+?>
