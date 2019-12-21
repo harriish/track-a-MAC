f5ce855743fb637360a6a6ea7f7127f2bfa099d8
ivaturi venkata harish kumar <43529736+harriish@users.noreply.github.com>
2019-12-21 19:06:52 +0100

    Update list.php

--- list.php
+++ list.php
@@ -5,7 +5,7 @@ include('config.php');
 $sqlite1 =<<<EOF
 	SELECT * from List;
 EOF;
-	$result = $db->http_build_query($sqlite1);
+	$result = $db->query($sqlite1);
 	while($row = $result->fetchArray(SQLITE3_ASSOC) ) {
 		echo  $row['DeviceIP']. "|" .$row['VLAN']. "|" .$row['PORT']. "|" .$row['MACS']. "\n";
 	}
