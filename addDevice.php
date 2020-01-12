669048d10cc90ec3738076204dfcb2d311b47b1d
ivaturi venkata harish kumar <43529736+harriish@users.noreply.github.com>
2020-01-12 14:13:07 +0100

    addDevice works

--- addDevice.php
+++ addDevice.php
@@ -2,33 +2,22 @@
 
 include('config.php');
 
-$ip = $_GET['ip']
+$ip = $_GET['ip'];
 $port = $_GET['port'];
 $community = $_GET['community'];
 $version = $_GET['version'];
 
 if(empty($ip) || empty($port) || empty($community) || empty($version)) {
-    echo "FALSE" ;   
+    echo "please provide proper input" ;   
 }
 
 else {
-    $run1 = $db->query('SELECT * FROM info');
-    $count = 0;
-    foreach ($run1 as $run1) {
-        if($run1['ip']==$ip && $run1['port']==$port && $run1['community']==$community && $run1['version']==$version){
-            $count = $count+1;
 
-        }
-    }
-
-    if($count==0){
-        $db->exec("INSERT INTO info (ip,port,community,version) VALUES ('$ip','$port','$community','$version')");
+    $db->exec("INSERT INTO info (IP,PORT,COMMUNITY,VERSION) VALUES ('$ip','$port','$community','$version')");
+        echo "\n";
         echo "OK";
+    
     }
-    else {
-        echo "FALSE";
-    }
-}
 $db->close();
 
 ?>
