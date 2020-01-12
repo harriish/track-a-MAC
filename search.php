8eafb99e2705f614749962e7e3497365843c839a
ivaturi venkata harish kumar <43529736+harriish@users.noreply.github.com>
2020-01-12 14:19:18 +0100

    search require mac address perfect

--- search.php
+++ search.php
@@ -1,48 +1,39 @@
 <?php
-include('config.php');
+include_once('config.php');
 
-if(empty($_GET)){
+if (empty($_GET)) {
     echo "It is False";
     }
 else {
-    $my_search_is = $_GET["mac"];
-
-
-    $sq1 = <<<EOF
-                SELECT * FROM List WHERE LIKE "%$my_search_is%" ORDER BY MACS;
+    $my_search_is = htmlspecialchars($_GET["mac"]);
+    $sql = <<<EOF
+              SELECT * FROM List WHERE MACS LIKE "%$my_search_is%" ORDER BY MACS;
 EOF;
-
-$my_output_is = $db->query($sq1ite1);
-$data = array();
-while($row = $my_output_is->fetchArray(SQLITE3_ASSOC) ) {
-
-
-        $data[] = $row['DeviceIP']. "|" .$row['VLAN']. "|" .$row['PORT']. "|" . "$my_search_is";
-    
+    $my_output_is = $db->query($sql);
+    $data = array(); 
+    while($row = $my_output_is->fetchArray(SQLITE3_ASSOC) ){
+         #echo $row[1]. "|" . $row[2] . "|" . $row[3] . "|" . $row[4] . "\n";
+         $data[] = $row['IP']. " | " . $row['VLANs'] . " | " . $row['PORT'] . " | " . "$my_search_is";
+     
     }
-
 
 $flag = count($data);
-if($flag == 0){
-
-
-
-    $count = $db->query('SELECT count(*) FROM info')
-    while($check = $count->fetchArray(SQLITE_ASSOC)) {
-        $number_of_devices = $check['count(*)'];#support wildcards
-        echo "we found no match in $number_of_devices devices";
-    }
+if($flag ==0){
+    $count = $db->query('SELECT count(*) FROM info');
+    while($check = $count->fetchArray(SQLITE3_ASSOC)) {
+        $number_of_devices = $check['count(*)'];
+        echo "wWe found no match in $number_of_devices devices"."\n";
+     }
 }
-
-
 
 $my_result_is = array_unique($data);
 $total = count($my_result_is);
-
-
-for($i=0; $i < $total; $i++){
+$i=0;
+while($i<$total){
     echo $my_result_is[$i]. "\n";
+    $i++;
     }
 }
 $db->close();
+
 ?>
